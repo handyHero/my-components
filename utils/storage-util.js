b99282e8505cf94df43eb083a47b41a910336b25
handyHero <1083479842@qq.com>
2020-12-09 22:51:26 +0800

    utils

---
+++ utils/storage-util.js
@@ -0,0 +1,67 @@
+const EXPIRE_THRESHOLD_TIME = (PRODUCTION ? 240 : 240) * 60 * 1000
+import _get from 'lodash/get'	//lodash处理,可直接用js
+import _isFunction from 'lodash/isFunction'
+import _isNumber from 'lodash/isNumber'
+
+const getItem = function(key) {
+    var storage = window.localStorage;
+    if (storage) {
+        var value = storage.getItem(key);
+        return value && JSON.parse(value);
+    }
+    return null;
+};
+
+const addItem = function(key, value) {
+    var storage = window.localStorage;
+    if (storage) {
+        storage.setItem(key, JSON.stringify({
+            value,
+            LS_TS: +new Date()
+        }));
+    }
+};
+
+const removeItem = function(key) {
+    var storage = window.localStorage;
+    if (storage) {
+        storage.removeItem(key);
+    }
+};
+
+const getItemValue = function(key) {
+    return _get(getItem(key), 'value')
+};
+
+const getItemTS = function(key) {
+    return _get(getItem(key), 'LS_TS')
+};
+
+const clearStorage = function() {
+    var storage = window.localStorage;
+    if (storage) {
+        storage.clear();
+    }
+};
+
+//检查登陆状态是否过期
+//使用localStorage保持会话，如果浏览器不支持该API，该系统的vue-router则会出现反复登录的死循环bug
+const checkItemExpire = function(key, cb) {
+    if (_isNumber(getItemTS(key)) && ((+new Date()) - getItemTS(key)) > EXPIRE_THRESHOLD_TIME) {
+        removeItem(key)
+        if (_isFunction(cb)) {
+            cb()
+        }
+        return true
+    }
+    return false
+}
+export {
+    getItem,
+    addItem,
+    removeItem,
+    getItemValue,
+    getItemTS,
+	clearStorage,
+    checkItemExpire
+}
---
+++ wx/wxapi/main.js
@@ -0,0 +1,62 @@
+const API_BASE_URL = 'https:111.com'
+let subDomain = '/api/v1'
+
+const request = (url, method, data, header = {}) => {
+	let _url = API_BASE_URL + subDomain + url
+	return new Promise((resolve, reject) => {
+		wx.request({
+			url: _url,
+			method: method,
+			data: data,
+			header: {
+				...header,
+				'Content-Type': 'application/json'
+			},
+			success(request) {
+				if(request.data.code === 0) {
+					resolve(request.data)
+				} else {
+					wx.showToast({
+						title: request.data.msg,
+						icon: 'none'
+					})
+				}
+			},
+			fail(error) {
+				reject(error)
+			},
+			complete(aaa) {
+				// 加载完成
+			}
+		})
+	})
+}
+
+Promise.prototype.finally = function(callback) {
+	var Promise = this.constructor;
+	return this.then(
+		function(value) {
+			Promise.resolve(callback()).then(
+				function() {
+					return value;
+				}
+			);
+		},
+		function(reason) {
+			Promise.resolve(callback()).then(
+				function() {
+					throw reason;
+				}
+			);
+		}
+	);
+}
+
+module.exports = {
+	request,
+	// 登录
+	login: data => request('/homework/login', 'post', data),
+	// 获取作业列表
+	getHomeworkList: (data) => request('/homework/list', 'post', data),
+
+}
